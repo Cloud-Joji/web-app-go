5acb253b29add1d1bcd70b3b7d2abd1afff517c1
Joji <joji.archcloud@gmail.com>
2022-11-18 11:07:28 -0400

    Proxy added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css'
 
 function App() {
-
-  var baseUrl = window.location.href
+  
+  
   
   return (
     <div>
       <h1>Hello World with Vite & Cloud Run!</h1>
       <button onClick={ async () => {
-        const response = await fetch(baseUrl + '/users')
+        const response = await fetch('/users')
         const data = await response.json()
         console.log(data)
-        console.log(baseUrl)
       }}>Get Data</button>
     </div>
   )
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-//	"os"
+	"os"
 	"fmt"
 //	"log"
 	"github.com/gofiber/fiber/v2"
@@ -15,8 +15,11 @@ func main(){
 	// getEnvs()
 
 	/* Setting the port */
-	// port := os.Getenv("PORT")
-	port := "4000"
+	port := os.Getenv("PORT")
+	
+	if port == ""{
+		port = "4000"
+	}
 
 	fmt.Println("Server listening on: " + port)
 	
